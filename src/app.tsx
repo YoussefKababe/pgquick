0bc956b8639168fa77fcbb8cce538d5d24bdad0f
YoussefKababe <youssef.kbe@gmail.com>
2024-09-05 16:46:01 +0200

    Add Tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('app'));
-root.render(<h2>Hello from React!</h2>);
+root.render(<h2 className='text-red-500'>Hello from React!</h2>);
